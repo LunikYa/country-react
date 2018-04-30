1a1dcc6d57cef2a990010c8ed99ef383a6c8afc3
Leonid <leonidyarmak@gmail.com>
2018-04-30 16:11:40 +0300

    fix

--- server/router.js
+++ server/router.js
@@ -1,13 +1,13 @@
 const Router = require('koa-router');
 const router = Router(); 
-const { getUsers, getUser, addNewUser, loginUser} = require('./userController');
-const {verifyToken }= require('./varifyToken')
+const { getUsers, getUser, addNewUser, loginUser} = require('./userModel');
+const { verifyToken }= require('./varifyToken')
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.post('/users/:name', addNewUser);
 router.post('/login', loginUser);
 
-router.use('/users', verifyToken);
+router.use(['/users', '/users/:id'], verifyToken);
 
 module.exports = router;
--- server/varifyToken.js
+++ server/varifyToken.js
@@ -1,5 +1,4 @@
 
-const jwtsecret = "countries-react-key";
 module.exports.verifyToken = async function (ctx, next) {
     const token = ctx.request.headers['x-access-token'];
 
@@ -7,7 +6,7 @@ module.exports.verifyToken = async function (ctx, next) {
         ctx.response.status = 403;
     ctx.response.body = { auth: false, message: 'No token provided.' };
 
-    jwt.verify(token, jwtsecret, (err, decoded) => {
+    jwt.verify(token, "countries-react-key", (err, decoded) => {
         if (err) {
             ctx.response.status = 404;
             ctx.response.body = { auth: false, message: 'Failed to authenticate token.' };
