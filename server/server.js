248b7e6d98375895a91a9f91b06af53fe248bac0
Leonid <leonidyarmak@gmail.com>
2018-05-02 18:22:44 +0300

    start with mongo

--- server/server.js
+++ server/server.js
@@ -6,6 +6,13 @@ const app = new Koa();
 const hostname = '127.0.0.1';
 const port = '3000';
 
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = "mongodb://localhost:27017/country";
+
+const deb = require('./mongo/db')
+// console.log('serve db', db)
+// require('./mongo/db')(app);
+
 app.use(bodyParser());
 
 app.use(async (ctx, next) => {
@@ -21,8 +28,28 @@ app.on('error', (err, ctx) => {
   log.error('server error', err, ctx)
 });
 
-app.listen(port, hostname, (e)=>{
-    if(e)
-        console.log(e)
-    console.log(`listen at http://${hostname}:${port}`)
+// app.listen(port, hostname, (e) => {
+//     if (e)
+//         console.log(e)
+//     console.log(`listen at http://${hostname}:${port}`)
+// });
+app.post('/test', (ctx, next) => {
+    console.log(db)
+})
+var db;
+MongoClient.connect(mongoUrl, function (err, database) {
+    if (err) console.log(err);
+
+    db = database;
+
+    console.log("Database connection ready");
+
+    app.listen(port, hostname, (e) => {
+        if (e)
+            console.log(e)
+        console.log(`listen at http://${hostname}:${port}`)
+    });
 });
+
+
+console.log('server', db)
