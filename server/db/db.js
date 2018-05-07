89d3efe290fb735e6fb12f47a1b745eede254982
Leonid <leonidyarmak@gmail.com>
2018-05-07 17:35:56 +0300

    ref async connect

--- server/db/db.js
+++ server/db/db.js
@@ -3,11 +3,13 @@ const mongoUrl    = "mongodb://localhost:27017/";
 
 var db;
 
-module.exports.connect = async function () {
+module.exports.connect = () => {
     return new Promise((res, rej) => {
-        MongoClient.connect(mongoUrl, async (err, client) => {
-            if (err) console.log(err)
-            db = await client.db('country-react');
+        MongoClient.connect(mongoUrl, (err, client) => 
+        {
+            if (err) console.log(err);
+            
+            db = client.db('country-react');
             res(db)
         });
     })
