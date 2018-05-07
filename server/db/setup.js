a038097a7bcc6708c002c98ad79a2e022a02071d
Leonid <leonidyarmak@gmail.com>
2018-05-07 18:11:15 +0300

    fix

--- server/db/setup.js
+++ server/db/setup.js
@@ -11,8 +11,8 @@ module.exports.initial = async () => {
     for (let key in file) {
         const cursor = await countries.insertOne({ name: key })
         
-        file[key].forEach(x => {
-            cities.insertOne({
+        file[key].forEach(async x => {
+            await cities.insertOne({
                 name: x,
                 countryId: cursor.ops[0]._id
             })
